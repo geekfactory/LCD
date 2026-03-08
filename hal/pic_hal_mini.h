8f24bf73128ec8074e47dac6288b30bc5115f37d
Jesus Ruben Santa Anna Zamudio <ruben@geekfactory.mx>
2026-03-08 17:51:15 -0600

    Change licence text to LGPL

--- hal/pic_hal_mini.h
+++ hal/pic_hal_mini.h
@@ -1,3 +1,22 @@
+/*	Geek Factory FlexLCD - Driver for HD44780 Compatible LCD Modules
+	Copyright (C) 2026 Jesus Ruben Santa Anna Zamudio.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published
+	by the Free Software Foundation; either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+	General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Author website: https://www.geekfactory.mx
+	Author e-mail: ruben at geekfactory dot mx
+ */
 #ifndef PIC_HAL_MINI_H
 #define PIC_HAL_MINI_H
 
